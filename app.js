55a3d564b1588ca41d649ca3b78cd342770b392f
sysadmin85 <servers@orionesolutions.com>
2016-07-12 13:31:06 +0530

    By Indy

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.listen(3000);
+});
+
+app.get('/test', function(req, res) {
+    res.status(200).send('Awesome!');
+});
+
+app.use("/", express.static(path.join(__dirname, 'public')));
